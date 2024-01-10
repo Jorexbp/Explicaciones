674d95fd9275e577688ac8b24031f120ca01fe39
Jorexbp <jorgebp2004@gmail.com>
2024-01-10 11:41:36 +0100

    Metodos inicio

---
+++ src/metodos/Metodos_Inicio.java
@@ -0,0 +1,25 @@
+package metodos;
+
+public class Metodos_Inicio {
+	
+	public static void felizCumpleaños() {
+		System.out.println("Te deseamos cumpleaños feliz");
+		System.out.println("Por ser un chico excelente");
+		System.out.println("Por ser un chico excelenteeeeee");
+		System.out.println("Y siempre lo será");
+	}
+
+	public static void main(String[] args) {
+		// ¿Qué son y para que sirven los métodos?
+
+		/*
+		 * Los métodos son partes de código que podemos almacenar para volver a ejecutar
+		 * esa misma parte y así evitar reescribir el mismo código.
+		 * 
+		 */
+		felizCumpleaños();
+		felizCumpleaños();
+		felizCumpleaños();
+
+	}
+}
