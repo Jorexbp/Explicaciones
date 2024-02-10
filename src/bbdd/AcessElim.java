0623ddea0d483310bffd15bc0b2d83f27d580f99
Jorexbp <jorgebp2004@gmail.com>
2024-02-10 10:45:55 +0100

    Deletge

--- src/bbdd/AcessElim.java
+++ src/bbdd/AcessElim.java
@@ -6,19 +6,20 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class AcessElim {
-    public static void main(String[] args) {
-        String dbUrl = "jdbc:ucanaccess://Ejemplo.accdb";
+	public static void main(String[] args) {
+		String dbUrl = "jdbc:ucanaccess://Ejemplo.accdb";
 
-        try (Connection connection = DriverManager.getConnection(dbUrl)) {
-             String sqlEliminacion = "DELETE FROM empleados WHERE id = ?";
-            try (PreparedStatement statement = connection.prepareStatement(sqlEliminacion)) {
-                statement.setInt(1, 1);
-                
-                int filasAfectadas = statement.executeUpdate();
-                System.out.println("Filas eliminadas: " + filasAfectadas);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+		try (Connection connection = DriverManager.getConnection(dbUrl)) {
+			String sqlEliminacion = "DELETE FROM empleados WHERE id = ?";
+			try (PreparedStatement statement = 
+					connection.prepareStatement(sqlEliminacion)) {
+				statement.setInt(1, 3);
+
+				int filasAfectadas = statement.executeUpdate();
+				System.out.println("Filas eliminadas: " + filasAfectadas);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
