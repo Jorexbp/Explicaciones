b14502da37174b8145c52b45fb2a174e9180ebb0
Jorexbp <jorgebp2004@gmail.com>
2024-02-10 10:56:39 +0100

    actus

--- src/bbdd/AccessEliminar.java
+++ src/bbdd/AccessEliminar.java
@@ -5,18 +5,21 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class AcessElim {
+public class AccessEliminar {
 	public static void main(String[] args) {
 		String dbUrl = "jdbc:ucanaccess://Ejemplo.accdb";
 
 		try (Connection connection = DriverManager.getConnection(dbUrl)) {
-			String sqlEliminacion = "DELETE FROM empleados WHERE id = ?";
-			try (PreparedStatement statement = 
-					connection.prepareStatement(sqlEliminacion)) {
-				statement.setInt(1, 6);
+			String sqlActualizacion = "UPDATE empleados"
+							+ " SET edad = ? WHERE nombre = ?";
+			try (PreparedStatement statement =
+						connection.prepareStatement(sqlActualizacion)) {
+				
+				statement.setInt(1, 40); // Nueva edad
+				statement.setString(2, "Luis"); // Nombre del empleado
 
 				int filasAfectadas = statement.executeUpdate();
-				System.out.println("Filas eliminadas: " + filasAfectadas);
+				System.out.println("Filas actualizadas: " + filasAfectadas);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
